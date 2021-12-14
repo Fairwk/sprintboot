9c4da2275c6051b1b6a9d36aaa042ed3cd10b2d2
王焜 <2267116985@qq.com>
2021-12-14 22:58:07 +0800

    这是第一个版本

--- src/day20211108/Test.java
+++ src/day20211108/Test.java
@@ -9,23 +9,28 @@ class Vehicle{
     }
 
     public int getWheels() {
+
         return wheels;
     }
 
     public void setWheels(int wheels) {
+
         this.wheels = wheels;
     }
 
     public double getWeight() {
+
         return weight;
     }
 
     public void setWeight(double weight) {
+
         this.weight = weight;
     }
     @Override
     public String toString(){
-        return "Vehicle[wheels = " + wheels + ", weight = " + weight + "]";
+        return "Vehicle[wheels = " + wheels + ", " +
+                "weight = " + weight + "]";
     }
 }
 class Car extends Vehicle{
@@ -43,7 +48,8 @@ class Car extends Vehicle{
     }
     @Override
     public String toString(){
-        return "Car[loader = " + loader + ", wheels = " + wheels + ", weight = " + weight + " ]";
+        return "Car[loader = " + loader + ", wheels = " +
+                wheels + ", weight = " + weight + " ]";
     }
 }
 public class Test {
