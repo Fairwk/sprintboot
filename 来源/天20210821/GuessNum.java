869eda165f7c881ff869bda621bdaac831839464
Fairwk <83054060+Fairwk@users.noreply.github.com>
2022-06-29 11:44:01 +0800

    Update and rename src/day20210821/GuessNum.java to 来源/天20210821/GuessNum.java

--- 来源/天20210821/GuessNum.java
+++ 来源/天20210821/GuessNum.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNum {
+    //打印输出一个整数的每一位
     public static void main5(String[] args) {
         System.out.println("请输入一个人整数:");
         Scanner sc = new Scanner(System.in);
@@ -18,10 +19,10 @@ public class GuessNum {
             System.out.println(count+"是"+i+"位数");
         } if(count <10){
             System.out.println(count+"是一位数:"+count);
-        }else{
-            for(k=i;k>0;k--){
+        }else{//这个else对应count<10，也就是说当数字不是一位数的时候进入else
+            for(k=i;k>0;k--){//循坏打印多位数的每一位
                 m=count;t=1;
-                for(j=0;j<k-1;j++){
+                for(j=0;j<k-1;j++){//当打印最后一位是不需要进入这个条件，所以j<k-1
                     t*=10;
                 }
                 m=m/t;m=m%10;
@@ -30,9 +31,10 @@ public class GuessNum {
         }
 
     }
+    //模拟登录
     public static void main4(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {//只有三次机会
             System.out.println("请输入密码:");
             String password = sc.nextLine();
             if ("abcde".equals(password)) {
@@ -41,11 +43,11 @@ public class GuessNum {
             }else if (i == 2) {
                 System.out.println("本次机会已用完请明天再来!");
             } else {
-                System.out.println("录入错误,还剩下" + (2 - i) + "次机会");
+                System.out.println("输入错误,还剩下" + (2 - i) + "次机会");
             }
         }
     }
-    public static void odd(int num){
+    public static void odd(int num){//打印奇数位
         for(int i = 30;i >= 0;i=i-2){
             if((num&(1 << i)) != 0){
                 System.out.println("1");
@@ -54,7 +56,8 @@ public class GuessNum {
             }
         }
     }
-    public static void even(int num){
+    public static void even(int num){//打印偶数位
+        //因为一共32位
         for(int i = 31;i >= 0;i=i-2){
             if((num & (1 << i))!=0){
                 System.out.println("1");
